cf8b9872717729fe7979ffba1f17c1ae7d9c7f07
Radu Lefter <lefter_radu@yahoo.com>
2024-02-02 14:52:55 +0000

    created search functionality for books on front end

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,13 +3,12 @@
 @section('content')
   <h1 class="mb-10 text-2xl">Books</h1>
 
-  {{-- <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+  <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
     <input type="text" name="title" placeholder="Search by title"
       value="{{ request('title') }}" class="input h-10" />
-    <input type="hidden" name="filter" value="{{ request('filter') }}" />
     <button type="submit" class="btn h-10">Search</button>
     <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
-  </form> --}}
+  </form>
 
   <ul>
     @forelse ($books as $book)
