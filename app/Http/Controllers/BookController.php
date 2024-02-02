ab8a126a1794a3ca5cd847806ca0142742271f8e
Radu Lefter <lefter_radu@yahoo.com>
2024-02-02 18:08:42 +0000

    created view for single book page

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -27,7 +27,7 @@ class BookController extends Controller
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
             default => $books->latest()
         };
-        
+
         $books = $books->get();
 
         return view('books.index', ['books' => $books]);
@@ -52,9 +52,16 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view(
+            'books.show',
+            [
+                'book' => $book->load([
+                    'reviews' => fn($query) => $query->latest()
+                ])
+            ]
+        );
     }
 
     /**
